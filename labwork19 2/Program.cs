2750e6261bc66945f80db37d2b5e2de23965ea02
barinskaya <1388zxc@gmail.com>
2022-12-29 11:25:25 +0300

    Добавьте файлы проекта.

---
+++ labwork19 2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("n = "); //задаем катеты
+        int n = int.Parse(Console.ReadLine()); //ввод числа с консоли
+        for (int i = 0; i < n; i++) //1 катет
+        {
+            for (int j = 0; j <= i; j++)//2 катет
+            {
+                Console.Write("m"); //символы, из которых состоит прямоугольный треугольник
+            }
+            Console.WriteLine(); //переход на следующую строку
+        }
+        Console.ReadLine();//программа ждет нажатия клавиши для завершения работы
+
+    }
+}
